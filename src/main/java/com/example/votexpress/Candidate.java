207fb354082e6dbf1e29f7f20099311cee627e6a
Franklin <writefranklyn@gmail.com>
2024-08-24 13:15:12 +0100

    create candidate entity class

--- src/main/java/com/example/votexpress/Candidate.java
+++ src/main/java/com/example/votexpress/Candidate.java
@@ -8,9 +8,43 @@ public class Candidate {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "party", nullable = false)
     private String party;
 
 
+    public Candidate() {}
+
+    public Candidate(String name, String party) {
+        this.name = name;
+        this.party = party;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getParty() {
+        return party;
+    }
+
+    public void setParty(String party) {
+        this.party = party;
+    }
 }
