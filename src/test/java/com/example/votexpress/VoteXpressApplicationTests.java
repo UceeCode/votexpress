908b84da69f5e67423c5a72e9a637aa64d53528c
Franklin <writefranklyn@gmail.com>
2024-08-24 11:46:38 +0100

    Initial commit

---
+++ src/test/java/com/example/votexpress/VoteXpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.votexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteXpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
