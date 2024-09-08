c6aaa4ff7bfa54e181b0817d49b162408fb17813
Franklin <writefranklyn@gmail.com>
2024-09-08 23:43:45 +0100

    create a repository layer called UserRepository and add a custom query to get user by email

---
+++ src/main/java/com/example/votexpress/Repositories/UserRepositories.java
@@ -0,0 +1,14 @@
+package com.example.votexpress.Repositories;
+
+import com.example.votexpress.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepositories extends JpaRepository<User, Long> {
+
+    //Query to find user by email
+    Optional<User> findByEmail(String username);
+}
