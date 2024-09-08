03ec21e09b4849349ae44ef227fd292c032d6857
Franklin <writefranklyn@gmail.com>
2024-09-09 00:03:29 +0100

    add VoteRepository and a custom method to find votes by user_id and election_id

---
+++ src/main/java/com/example/votexpress/Repositories/VoteRepository.java
@@ -0,0 +1,17 @@
+package com.example.votexpress.Repositories;
+
+import com.example.votexpress.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+    // Custom query to find votes by user ID
+    List<Vote> findByUser_Id(Long id);
+
+    // Custom query to find votes by election ID
+    List<Vote> findByElection_Id(Long id);
+}
