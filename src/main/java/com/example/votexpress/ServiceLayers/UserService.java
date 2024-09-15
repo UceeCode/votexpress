ecfe9896b5816e7f9c1396074c5204e9c331bb9d
Franklin <writefranklyn@gmail.com>
2024-09-15 23:41:53 +0100

    create user service layer

---
+++ src/main/java/com/example/votexpress/ServiceLayers/UserService.java
@@ -0,0 +1,36 @@
+package com.example.votexpress.ServiceLayers;
+
+import com.example.votexpress.Repositories.UserRepositories;
+import com.example.votexpress.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepositories userRepositories;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepositories userRepositories, PasswordEncoder passwordEncoder) {
+        this.userRepositories = userRepositories;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    //Register a user
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepositories.save(user);
+    }
+
+    //Find a user by email
+    public Optional<User> findByUserByEmail(String email) {
+        return userRepositories.findByEmail(email);
+    }
+
+    //Find a user by id
+    public  User getUserById(long id){
+        return userRepositories.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+}
