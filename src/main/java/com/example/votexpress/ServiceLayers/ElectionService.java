47aa94fc869d1cee3a10352a19c570dff3df071e
Franklin <writefranklyn@gmail.com>
2024-09-15 23:32:13 +0100

    add election service layer

---
+++ src/main/java/com/example/votexpress/ServiceLayers/ElectionService.java
@@ -0,0 +1,29 @@
+package com.example.votexpress.ServiceLayers;
+
+import com.example.votexpress.Election;
+import com.example.votexpress.Repositories.ElectionRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ElectionService {
+    private final ElectionRepository electionRepository;
+    public ElectionService(ElectionRepository electionRepository) {
+        this.electionRepository = electionRepository;
+    }
+
+    //Create an election
+    public Election CreateElection(Election election) {
+        return electionRepository.save(election);
+    }
+
+    //Find an Election by name
+    public Election getElectionName(String name){
+        return electionRepository.findByname(name);
+    }
+
+    //find an election by id
+    public Election getElectionById(long id){
+        return electionRepository.findById(id).orElseThrow(() ->
+        new RuntimeException("Election not found"));
+    }
+}
