5362202427f276c3ceb4dac3b09803dec69e488f
Franklin <writefranklyn@gmail.com>
2024-09-15 23:50:40 +0100

    create vote service layer

---
+++ src/main/java/com/example/votexpress/ServiceLayers/VoteService.java
@@ -0,0 +1,30 @@
+package com.example.votexpress.ServiceLayers;
+
+import com.example.votexpress.Repositories.VoteRepository;
+import com.example.votexpress.Vote;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VoteService {
+    private final VoteRepository voteRepository;
+    public VoteService(VoteRepository voteRepository) {
+        this.voteRepository = voteRepository;
+    }
+
+    //Submit a vote
+    public Vote SubmitVote(Vote vote) {
+        return voteRepository.save(vote);
+    }
+
+    //find votes by user id
+    public List<Vote> getVotesByUserId(long userId) {
+        return voteRepository.findByUser_Id(userId);
+    }
+
+    //find votes by election id
+    public List<Vote> getVotesByElectionId(long electionId) {
+        return voteRepository.findByElection_Id(electionId);
+    }
+}
